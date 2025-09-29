c0c1a0dd8072735ae50a4be6be67d8660f79c7c7
acpilot 622 <yokoshima.auharing@outlook.com>
2025-09-29 23:07:10 +0800

    update some scripts for easier use

--- model_realtimeeval.py
+++ model_realtimeeval.py
@@ -4,14 +4,21 @@ from  pathlib import Path
 import cv2
 # This is a memory sensitive task, so CLOSE YOUR BROWSER before running this script!!!!!!
 # your browser can occupy more than 10 GB memory, so remember to close it before you run this script!
+# you can press q to exit
 
-def realtime_eval_og():
+#if you choose a video then it says: [ WARN:0@10.870] global cap_msmf.cpp:936 CvCapture_MSMF::initStream Failed to set mediaType (stream 0, (3840x2160 @ 29.97) MFVideoFormat_RGB32(codec not found)
+#then you have to close the terminal. This is because Opencv don't support rgb32 and 4k res video..........
+
+tank_detect_model = "tanks"
+drone_detect_model = "drones"
+
+def realtime_eval_og(model_type, url):
     currentdir = Path(".")
-    modellocation = currentdir / "models" / "tanks" / "best.pt"
+    modellocation = currentdir / "models" / model_type / "best.pt"
 
     model = YOLO(modellocation)  
 
-    online_url = "https://youtu.be/sj5ufK4epmA"
+    online_url = url
     # 直接预测视频文件
     results = model.predict(
         source=online_url, 
@@ -23,16 +30,16 @@ def realtime_eval_og():
 
     )
 
-def realtime_eval_cv():
+def realtime_eval_cv(model_type, url):
     currentdir = Path(".")
-    modellocation = currentdir / "models" / "tanks" / "best.pt"
+    modellocation = currentdir / "models" / model_type / "best.pt"
 
     model = YOLO(modellocation)  
-    online_url = "https://youtu.be/sj5ufK4epmA"
+    online_url = url
     results = model.predict(
         source=online_url,
         stream=True,
-        conf=0.5,
+        conf=0.4, ## set a smaller value if you want to detected approximately. the smaller the value, the more possibility for bounding tanks
         save=False,
     )
 
@@ -51,6 +58,14 @@ if __name__ == "__main__":
     os.environ["http_proxy"] = proxy_url
     os.environ["https_proxy"] = proxy_url
 
-#    realtime_eval_og()
-    realtime_eval_cv()
+    video_url = ["https://youtu.be/sj5ufK4epmA",  \
+        #this is tank video
+        "https://youtu.be/ambp_X7m-2s",\
+        #tanks video 2
+        "https://youtu.be/1U7Bu2pSUwU",\
+        #this is heli video
+                 ]
 
+#    realtime_eval_og()
+
+    realtime_eval_cv(tank_detect_model, video_url[1])
